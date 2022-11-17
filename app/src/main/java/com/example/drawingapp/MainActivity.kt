f8b8dae95beac8ea559afc0dafba2f803b19e8ed
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2022-11-18 03:02:02 +0530

    added comments and finalized app

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -16,7 +16,6 @@ import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -38,12 +37,11 @@ class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint: ImageButton? = null
     private var customProgressDialog: Dialog? = null
 
-    private val openGalleryLauncher : ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result ->
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             // resultCode == RESULT_OK checks if we get the data back
-            if (result.resultCode == RESULT_OK && result.data!=null){
-                val imageBackGround : ImageView = findViewById(R.id.iv_background)
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackGround: ImageView = findViewById(R.id.iv_background)
                 imageBackGround.setImageURI(result.data?.data)
                 /* result.data will give the URI (location path) of the resultant image that
                 we have selected and result.data?.data will give us the image.
@@ -54,9 +52,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    /** create an ActivityResultLauncher with MultiplePermissions since we are requesting
+     * both read and write
+     */
     private val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
             permissions.entries.forEach {
                 val permissionName = it.key
                 val isGranted = it.value    // Boolean value
@@ -118,15 +120,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         val ibRedo: ImageButton = findViewById(R.id.ib_redo)
-        ibRedo.setOnClickListener{
+        ibRedo.setOnClickListener {
             drawingView?.onClickRedo()
         }
 
         val ibSave: ImageButton = findViewById(R.id.ib_save)
-        ibSave.setOnClickListener{
-            if(isReadStorageAllowed()){
+        ibSave.setOnClickListener {
+            if (isReadStorageAllowed()) {
                 showProgressDialog()
-                lifecycleScope.launch{
+                lifecycleScope.launch {
                     // Gets the Frame layout
                     val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
 
@@ -147,6 +149,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Method is used to launch the dialog to select different brush sizes.
+     */
     private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
@@ -182,6 +187,11 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
+    /**
+     * Method is called when color is clicked from pallet_normal.
+     *
+     * @param view ImageButton on which click took place.
+     */
     fun paintClicked(view: View) {
         if (view != mImageButtonCurrentPaint) {   // Checks if we clicked on a different paint
             val imageButton = view as ImageButton  //Typecasts the view to an ImageButton
@@ -202,24 +212,52 @@ class MainActivity : AppCompatActivity() {
                 // Changes the border of previously selected paint to normal
             )
 
+            //Current view is updated with selected view in the form of ImageButton.
             mImageButtonCurrentPaint = view
         }
     }
 
-    private fun isReadStorageAllowed() : Boolean {
+    /**
+     * We are calling this method to check the permission status
+     */
+    private fun isReadStorageAllowed(): Boolean {
+        //Getting the permission status
+        // Here the checkSelfPermission is
+        /**
+         * Determine whether <em>you</em> have been granted a particular permission.
+         *
+         * @param permission The name of the permission being checked.
+         *
+         */
         val result =
             ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        /**
+         *
+         * @return {@link android.content.pm.PackageManager#PERMISSION_GRANTED} if you have the
+         * permission, or {@link android.content.pm.PackageManager#PERMISSION_DENIED} if not.
+         *
+         */
+        //If permission is granted returning true and If permission is not granted returning false
         return result == PackageManager.PERMISSION_GRANTED
     }
 
+    //create a method to requestStorage permission
     private fun requestStoragePermission() {
+        // Check if the permission was denied and show rationale
         if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                this, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            //call the rationale dialog to tell the user why they need to allow permission request
             showRationaleDialog(
                 "Drawing App",
                 "Drawing App needs access to External Storage for setting background image"
             )
         } else {
+            // You can directly ask for the permission.
+            //if it has not been denied then request for permission
+            //  The registered ActivityResultCallback gets the result of this request.
             requestPermission.launch(
                 arrayOf(
                     Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -229,32 +267,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**  create rationale dialog
+     * Shows rationale dialog for displaying why the app needs permission
+     * Only shown if the user has denied the permission request previously
+     */
     private fun showRationaleDialog(title: String, message: String) {
         val builder = AlertDialog.Builder(this)
-        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel"){
-                dialog, _ ->dialog.dismiss()
+        builder.setTitle(title).setMessage(message).setPositiveButton("Cancel") { dialog, _ ->
+            dialog.dismiss()
         }
         builder.create().show()
     }
 
-    private fun getBitmapFromView(view: View) : Bitmap {
+    /**
+     * Create bitmap from view and returns it
+     */
+    private fun getBitmapFromView(view: View): Bitmap {
         // The bitmap is the background image, the view which contains our canvas - where we can paint on
         // So, the bitmap is basically an image sandwich made of these components
+
+        //Define a bitmap with the same size as the view.
+        // CreateBitmap : Returns a mutable bitmap with the specified width and height
         val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
         val canvas = Canvas(returnedBitmap)
+        //Get the view's background
         val bgDrawable = view.background
 
-        if (bgDrawable != null){
+        if (bgDrawable != null) {
+            //has background drawable, then draw it on the canvas
             bgDrawable.draw(canvas)
-        } else{
+        } else {
+            //does not have background drawable, then draw black background on the canvas
             canvas.drawColor(Color.BLACK)
         }
 
+        // draw the view on the canvas
         view.draw(canvas)
+        //return the bitmap
         return returnedBitmap
     }
 
-    private suspend fun saveBitmapFile(mBitmap: Bitmap?) : String {
+    private suspend fun saveBitmapFile(mBitmap: Bitmap?): String {
         var result = ""
         withContext(Dispatchers.IO) {
             if (mBitmap != null) {
@@ -263,8 +317,24 @@ class MainActivity : AppCompatActivity() {
                     // Buffer capacity is initially 32 bytes, but can be increased if necessary
                     val bytes = ByteArrayOutputStream()
 
-                    // The bitmap is compressed into a png formal with 90% quality
                     mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+                    /**
+                     * Write a compressed version of the bitmap to the specified outputstream.
+                     * If this returns true, the bitmap can be reconstructed by passing a
+                     * corresponding inputstream to BitmapFactory.decodeStream(). Note: not
+                     * all Formats support all bitmap configs directly, so it is possible that
+                     * the returned bitmap from BitmapFactory could be in a different bitdepth,
+                     * and/or may have lost per-pixel alpha (e.g. JPEG only supports opaque
+                     * pixels).
+                     *
+                     * @param format   The format of the compressed image
+                     * @param quality  Hint to the compressor, 0-100. 0 meaning compress for
+                     *                 small size, 100 meaning compress for max quality. Some
+                     *                 formats, like PNG which is lossless, will ignore the
+                     *                 quality setting
+                     * @param stream   The outputstream to write the compressed data.
+                     * @return true if successfully compressed to the specified stream.
+                     */
 
                     // f is file in which the png format compressed bitmap is stored
                     // here externalCacheDir?.absoluteFile.toString() is the file path
@@ -273,14 +343,17 @@ class MainActivity : AppCompatActivity() {
                                 + File.separator + "DrawingApp_"
                                 + System.currentTimeMillis() / 1000 + ".png"
                     )
-                    /* System.currentTimeMillis() / 1000 gives the current time in milliseconds
-                    / divided by 1000 - used in order to give uniqueness to the file name*/
+                    // Here the Environment : Provides access to environment variables.
+                    // getExternalStorageDirectory : returns the primary shared/external storage directory.
+                    // absoluteFile : Returns the absolute form of this abstract pathname.
+                    // File.separator : The system-dependent default name-separator character. This string contains a single character.
 
-                    val fo = FileOutputStream(f)
-                    fo.write(bytes.toByteArray())
-                    fo.close()
-
-                    result = f.absolutePath
+                    val fo =
+                        FileOutputStream(f) // Creates a file output stream to write to the file represented by the specified object.
+                    fo.write(bytes.toByteArray()) // Writes bytes from the specified byte array to this file output stream.
+                    fo.close() // Closes this file output stream and releases any system resources associated with this stream. This file output stream may no longer be used for writing bytes.
+                    result = f.absolutePath // The file absolute path is return as a result.
+                    //We switch from io to ui thread to show a toast
 
                     runOnUiThread {
                         cancelProgressDialog()
@@ -309,7 +382,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Progress dialog to run while the image is being saved
-    private fun showProgressDialog(){
+    private fun showProgressDialog() {
         customProgressDialog = Dialog(this@MainActivity)
 
         /* Set the screen content from a layout resource.
@@ -321,11 +394,54 @@ class MainActivity : AppCompatActivity() {
         customProgressDialog?.show()
     }
 
-    // Cancelling the progress dialog
-    private fun cancelProgressDialog(){
-        if (customProgressDialog != null){
+    /**
+     * This function is used to dismiss the progress dialog if it is visible to user.
+     */
+    private fun cancelProgressDialog() {
+        if (customProgressDialog != null) {
             customProgressDialog?.dismiss()
             customProgressDialog = null
         }
     }
 }
+    /*
+    private fun shareImage(result: String) {
+        /*MediaScannerConnection provides a way for applications to pass a
+      newly created or downloaded media file to the media scanner service.
+      The media scanner service will read metadata from the file and add
+      the file to the media content provider.
+      The MediaScannerConnectionClient provides an interface for the
+      media scanner service to return the Uri for a newly scanned file
+      to the client of the MediaScannerConnection class.*/
+
+        /*scanFile is used to scan the file when the connection is established with MediaScanner.*/
+        MediaScannerConnection.scanFile(
+            this@MainActivity, arrayOf(result), null
+        ) { path, uri ->
+            // This is used for sharing the image after it has being stored in the storage.
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(
+                Intent.EXTRA_STREAM,
+                uri
+            ) // A content: URI holding a stream of data associated with the Intent, used to supply the data being sent.
+            shareIntent.type =
+                "image/png" // The MIME type of the data being handled by this intent.
+            startActivity(
+                Intent.createChooser(
+                    shareIntent,
+                    "Share"
+                )
+            )// Activity Action: Display an activity chooser,
+            // allowing the user to pick what they want to before proceeding.
+            // This can be used as an alternative to the standard activity picker
+            // that is displayed by the system when you try to start an activity with multiple possible matches,
+            // with these differences in behavior:
+        }
+        // END
+    }
+    /**
+     * Method is used to show the Custom Progress Dialog.
+     */
+}
+*/
